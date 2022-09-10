37170a299bec4c7689f318622c4c6e791f5d49cc
Timothy Beler <timothybeler4@gmail.com>
2022-09-10 07:10:20 -0400

    Added DOM manipulation for all the buttons

--- app.js
+++ app.js
@@ -1,10 +1,95 @@
+// @coollapse
 // If numlock is turned off the keypad should be visibly disabled.
+let curNum = document.getElementById("ans");
+let prevNum = document.getElementById("operation");
+let operationState = "NA";
+// buttons
+const clearBtn = document.getElementById("clear");
+const deleteBtn = document.getElementById("delete");
+const multiplyBtn = document.getElementById("operator-multiply");
+const divideBtn = document.getElementById("operator-divide");
+const minusBtn = document.getElementById("operator-minus");
+const plusBtn = document.getElementById("operator-plus");
+const decimalBtn = document.getElementById("decimal");
+const enterBtn = document.getElementById("enter");
+const num = document.getElementById("");
 
-let numberSelected = document.getElementById("number");
-// const updateDisplay = function (...args) {
-// 	document.getElementByClass(operation).innerHTML = args;
-// };
+function clear() {
+	prevNum.innerHTML = "";
+	curNum.innerHTML = "0";
+	operationState = "NA";
+}
 
+function evaluate(equation) {
+	return equation;
+}
+
+class Calculator {
+	constructor(prevNum, curNum) {
+		this.prevNum = prevNum;
+		this.curNum = curNum;
+		clear();
+	}
+}
+
+// Button action Listeners
+// Clear, delete, and enter
+clearBtn.addEventListener("click", () => {
+	clear();
+});
+deleteBtn.addEventListener("click", () => {
+	curNum.innerHTML = "0";
+});
+enterBtn.addEventListener("click", () => {
+	if (operationState == "NA") return;
+
+	prevNum.innerHTML =
+		" " + " = " + evaluate(prevNum.innerHTML) + " " + curNum.innerHTML;
+	curNum.innerHTML = "0";
+});
+// operator buttons
+multiplyBtn.addEventListener("click", () => {
+	if (curNum.innerHTML == "0") return;
+	operationState = multiplyBtn.innerText;
+	prevNum.innerHTML += " " + curNum.innerHTML + " " + operationState;
+	curNum.innerHTML = "0";
+});
+divideBtn.addEventListener("click", () => {
+	if (curNum.innerHTML == "0") return;
+
+	operationState = divideBtn.innerText;
+	prevNum.innerHTML += " " + curNum.innerHTML + " " + operationState;
+	curNum.innerHTML = "0";
+});
+plusBtn.addEventListener("click", () => {
+	if (curNum.innerHTML == "0") return;
+	operationState = plusBtn.innerText;
+	prevNum.innerHTML += " " + curNum.innerHTML + " " + operationState;
+	curNum.innerHTML = "0";
+});
+minusBtn.addEventListener("click", () => {
+	if (curNum.innerHTML == "0") return;
+	operationState = minusBtn.innerText;
+	prevNum.innerHTML += " " + curNum.innerHTML + " " + operationState;
+	curNum.innerHTML = "0";
+});
+// Number buttons
+document.querySelectorAll(".numberBtn").forEach((item) => {
+	console.log("entered logic for number");
+	item.addEventListener("click", () => {
+		let numVal = item.textContent;
+		if (curNum.innerHTML == "0") curNum.innerHTML = numVal;
+		else curNum.innerHTML += numVal;
+	});
+});
+decimalBtn.addEventListener("click", () => {
+	let decimalVal = decimalBtn.textContent;
+	if (curNum.innerHTML.includes(".")) return;
+	else {
+		curNum.innerHTML += decimalVal;
+	}
+});
+// Calculator Functions
 const add = function (...args) {
 	let j = 0;
 	let sumNum = 0;
