181c48a8244dcca21c27cd4f7fdddac843f38bf8
Timothy Beler <timothybeler4@gmail.com>
2022-09-11 07:06:06 -0400

    Added more logic to the calcultor. Still buggy

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-// @coollapse
+// @collapse
 // If numlock is turned off the keypad should be visibly disabled.
 let curNum = document.getElementById("ans");
 let prevNum = document.getElementById("operation");
-let operationState = "NA";
+// let operationState = "NA";
+// let numbers = [];
+// let equations = [];
+let multOrDivide = 0;
+let eqArray = [];
+
 // buttons
 const clearBtn = document.getElementById("clear");
 const deleteBtn = document.getElementById("delete");
@@ -18,19 +23,54 @@ function clear() {
 	prevNum.innerHTML = "";
 	curNum.innerHTML = "0";
 	operationState = "NA";
+	numbers = [];
+	equations = [];
+	eqArray = [];
+	multOrDivide = 0;
 }
 
-function evaluate(equation) {
-	return equation;
-}
+function evaluate() {
+	let num1 = 0;
+	// let num2 = 0;
+	answer = 0;
+	for (let i = 0; i < eqArray.length; i++) {
+		if (eqArray[i] != NaN) {
+			num1 = Number(eqArray[i]);
+		}
+		if (eqArray[i] == "+") {
+			answer += sum(num1, Number(eqArray[i + 1]));
+		}
+	}
 
-class Calculator {
-	constructor(prevNum, curNum) {
-		this.prevNum = prevNum;
-		this.curNum = curNum;
-		clear();
+	return answer;
+}
+function addToArray(varOne, varTwo) {
+	//Accepts two variables. If it's subtracted one of the variables will be a negative value (varTwo). Otherwise it should be just two regular old values.
+
+	if (varTwo == NaN) {
+		eqArray.push(varOne);
+		return;
+	}
+	if (multOrDivide == 1) {
+		eqArray.push(varOne * varTwo);
+		multOrDivide = 0;
+	}
+	if (multOrDivide == 2) {
+		eqArray.push(varOne / varTwo);
+		multOrDivide = 0;
+	} else {
+		eqArray.push(varOne);
+		eqArray.push(varTwo);
 	}
 }
+
+// class Calculator {
+// 	constructor(prevNum, curNum) {
+// 		this.prevNum = prevNum;
+// 		this.curNum = curNum;
+// 		clear();
+// 	}
+// }
 
 // Button action Listeners
 // Clear, delete, and enter
@@ -41,41 +81,70 @@ deleteBtn.addEventListener("click", () => {
 	curNum.innerHTML = "0";
 });
 enterBtn.addEventListener("click", () => {
-	if (operationState == "NA") return;
+	prevNum.innerHTML = " " + prevNum.innerHTML + " " + curNum.innerHTML + " = ";
+	let num = Number(curNum.innerHTML);
+	if (multOrDivide == 1) {
+		let num2 = eqArray.pop();
+		num = multiply(num2, Number(curNum.innerHTML));
+	}
 
-	prevNum.innerHTML =
-		" " + " = " + evaluate(prevNum.innerHTML) + " " + curNum.innerHTML;
+	eqArray.push(num);
 	curNum.innerHTML = "0";
+	console.log(eqArray);
 });
 // operator buttons
 multiplyBtn.addEventListener("click", () => {
-	if (curNum.innerHTML == "0") return;
-	operationState = multiplyBtn.innerText;
-	prevNum.innerHTML += " " + curNum.innerHTML + " " + operationState;
+	multOrDivide = 1;
+	// let num1 = 0;
+
+	// let num1 = eqArray[eqArray.length];
+	let num2 = Number(curNum.innerHTML);
+	// let ans = 0;
+	// ans = multiply(num1, num2);
+	// // if (curNum.innerHTML == "0") return;
+	eqArray.push(num2);
+	// eqArray.push("x");
+	prevNum.innerHTML += " " + curNum.innerHTML + " " + "x";
 	curNum.innerHTML = "0";
 });
 divideBtn.addEventListener("click", () => {
 	if (curNum.innerHTML == "0") return;
 
 	operationState = divideBtn.innerText;
-	prevNum.innerHTML += " " + curNum.innerHTML + " " + operationState;
+	prevNum.innerHTML += " " + curNum.innerHTML + " " + "/";
+	eqArray.push(Number(curNum.innerHTML));
+	// eqArray.push("/");
+
 	curNum.innerHTML = "0";
 });
 plusBtn.addEventListener("click", () => {
+	// let num = Number(curNum.innerHTML);
 	if (curNum.innerHTML == "0") return;
-	operationState = plusBtn.innerText;
-	prevNum.innerHTML += " " + curNum.innerHTML + " " + operationState;
+
+	addToArray(Number(curNum.innerHTML), eqArray.pop());
+	prevNum.innerHTML += " " + curNum.innerHTML + " " + "+";
+	// eqArray.push(num);
+	// eqArray.push("+");
+
 	curNum.innerHTML = "0";
 });
 minusBtn.addEventListener("click", () => {
+	let num = Number(curNum.innerHTML);
+	if (multOrDivide == 1) {
+		let num2 = eqArray.pop();
+		num = multiply(num2, Number(curNum.innerHTML));
+		multOrDivide = 0;
+	}
 	if (curNum.innerHTML == "0") return;
-	operationState = minusBtn.innerText;
-	prevNum.innerHTML += " " + curNum.innerHTML + " " + operationState;
+	prevNum.innerHTML += " " + curNum.innerHTML + " " + "-";
+	// num = num * -1;
+	eqArray.push(num);
+	// eqArray.push("-");
+
 	curNum.innerHTML = "0";
 });
 // Number buttons
 document.querySelectorAll(".numberBtn").forEach((item) => {
-	console.log("entered logic for number");
 	item.addEventListener("click", () => {
 		let numVal = item.textContent;
 		if (curNum.innerHTML == "0") curNum.innerHTML = numVal;
@@ -95,6 +164,7 @@ const add = function (...args) {
 	let sumNum = 0;
 	while (j < args.length) {
 		sumNum += args[j];
+		// console.log(sumNum);
 		j++;
 	}
 	return sumNum;
@@ -121,14 +191,8 @@ const sum = function (args) {
 	return sumNum;
 };
 
-const multiply = function (args) {
-	let j = 0;
-	let product = 1;
-	args.forEach((element) => {
-		product *= element;
-	});
-
-	return product;
+const multiply = function (num1, num2) {
+	return num1 * num2;
 };
 
 const power = function (base, exponent) {
